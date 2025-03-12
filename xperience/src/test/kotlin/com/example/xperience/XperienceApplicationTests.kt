a574e43c09057b5b6169105e100f101154ecfdc3
Lucas Gabriel de Resende <92735087+Lucasgrse@users.noreply.github.com>
2025-03-12 19:19:05 -0300

    fear: initial starting project

---
+++ xperience/src/test/kotlin/com/example/xperience/XperienceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.xperience
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class XperienceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
