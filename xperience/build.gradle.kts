2f4dcf3f14d22458e4fe4c30bc776ebd500ada41
Lucas Gabriel de Resende <92735087+Lucasgrse@users.noreply.github.com>
2025-03-12 19:25:34 -0300

    feat: created docker compose to we use database and updated libraries for we manage the project

--- xperience/build.gradle.kts
+++ xperience/build.gradle.kts
@@ -19,12 +19,24 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.flywaydb:flyway-core")
+	implementation("io.jsonwebtoken:jjwt-api")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.springdoc:springdoc-openapi-ui")
+	implementation("org.springdoc:springdoc-openapi-kotlin")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	runtimeOnly ("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly ("io.jsonwebtoken:jjwt-jackson:0.11.5")
+	runtimeOnly("org.postgresql:postgresql")
 }
 
 kotlin {
